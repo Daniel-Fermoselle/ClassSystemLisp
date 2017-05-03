1f8f220b911b112615bfd19ef55f3c5570696950
TiaGoMSR <tiagomsr4@gmail.com>
2017-05-03 09:52:53 +0100

    Constructors and getters working

---
+++ project.lisp
@@ -0,0 +1,34 @@
+(defparameter classes (make-hash-table))
+
+(defun create-constructor (slots constructor classes-names)
+  `(defun ,constructor (&key ,@slots)
+     (let ((instance (make-hash-table)))
+       (progn
+         (setf (gethash 'slots instance) (vector ,@slots))
+         (setf (gethash 'instance-of instance) (car ',classes-names))
+         (return-from ,constructor instance)))))
+
+(defun index-list (list length)
+  (let ((indexes (loop for x from 0 to length collect (car (list x)))))
+    (mapcar #'cons list indexes)))
+
+(defun create-getter (getter-index)
+  `(defun ,(car getter-index) (instance)
+     (aref (gethash 'slots instance) ,(cdr getter-index))))
+
+(defmacro def-class (classes-names &rest slots)
+  (let
+      ((constructor
+         (intern (concatenate 'STRING
+                              "MAKE" "-" (symbol-name (car classes-names)))))
+       (getters
+         (index-list
+          (mapcar #'(lambda (x) (intern (concatenate 'STRING
+                                                     (symbol-name (car classes-names)) "-" (symbol-name x)))) slots)
+          (- (length slots) 1)))
+       (recognizer
+         (intern (concatenate 'STRING
+                              (symbol-name (car classes-names)) "?"))))
+    `(progn
+       ,(create-constructor slots constructor classes-names)
+       (progn ,@(mapcar 'create-getter getters)))))
