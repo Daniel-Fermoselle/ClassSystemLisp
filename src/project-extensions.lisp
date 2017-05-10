ffc728f9d771a5661a147df5a28d8b979d0fcb25
Daniel-Fermoselle <daniel.fermoselle@gmail.com>
2017-05-10 20:08:55 +0100

    Classes now cannot inherit from themselves, also setters have now !

--- src/project-extensions.lisp
+++ src/project-extensions.lisp
@@ -4,21 +4,28 @@
 (defun create-hashtable (alist)
   (let ((ht (make-hash-table)))
     (loop for (key . value) in alist
-          do (setf (gethash key ht) (car value)))
+       do (setf (gethash key ht) (car value)))
     ht))
 
-
-(defun add-to-class-system (class-name slots precedence-list)
+;;;;;
+(defun add-to-class-system (class-name slots precedence-list precedence-list2)
   `(let ((class (make-hash-table)))
      (progn
-       (setf (gethash 'is-a class) ',precedence-list)
-       (setf (gethash 'slot-names class) ',slots)
-       (setf (gethash ',class-name class-system) class))))
+       (if
+        (eql (find ',class-name (rest ',precedence-list2)) nil)
+	(progn
+	  (setf (gethash 'is-a class) ',precedence-list)
+	  (setf (gethash 'slot-names class) ',slots)
+	  (setf (gethash ',class-name class-system) class)
+	  )
+	(error "~S cannot inherit from itself." ',class-name)
+	))))
+;;;;;;
 
 
 (defun create-constructor (class-name slots slots-names)
   (let ((constructor
-          (intern (concatenate 'STRING  "MAKE" "-" (symbol-name class-name)))))
+	 (intern (concatenate 'STRING  "MAKE" "-" (symbol-name class-name)))))
     `(defun ,constructor (&key ,@slots-names)
        (let* ((instance (make-hash-table))
               (slot-values (list ,@slots))
@@ -32,7 +39,7 @@
 
 (defun create-getter (class-name slot)
   (let ((getter
-          (intern (concatenate 'STRING  (symbol-name class-name) "-" (symbol-name slot)))))
+	 (intern (concatenate 'STRING  (symbol-name class-name) "-" (symbol-name slot)))))
     `(defun ,getter (instance)
        (multiple-value-bind
              (ret exist?)
@@ -43,14 +50,14 @@
 
 (defun create-setter (class-name slot)
   (let* ((setter
-           (intern (concatenate 'STRING  "SET" "-" (symbol-name class-name) "-" (symbol-name slot)))))
+	  (intern (concatenate 'STRING  (symbol-name class-name) "-" (symbol-name slot) "!"))));;;;;;;
     `(defun ,setter (instance value)
        (setf (gethash ',slot (gethash 'slots instance)) value))))
 
 
 (defun create-recognizer (class-name)
   (let ((recognizer
-          (intern (concatenate 'STRING (symbol-name class-name) "?"))))
+	 (intern (concatenate 'STRING (symbol-name class-name) "?"))))
     `(defun ,recognizer (instance)
        (if
         (eql (find ',class-name (gethash 'is-a (gethash 'instance-of instance))) nil)
@@ -60,7 +67,7 @@
 
 (defun get-class-slots (classes slots)
   (let* ((superclasses-slots
-           (mapcar #'(lambda (x) (gethash 'slot-names (gethash x class-system))) classes))
+	  (mapcar #'(lambda (x) (gethash 'slot-names (gethash x class-system))) classes))
          (appended-slots (apply #'append superclasses-slots)))
     (remove-duplicates (append slots appended-slots)
                        :test #'eql
@@ -73,37 +80,51 @@
 
 (defun get-precedence-list (classes)
   (let* ((superclasses-names
-           (mapcar #'(lambda (x) (gethash 'is-a (gethash x class-system))) (rest classes)))
+	  (mapcar #'(lambda (x) (gethash 'is-a (gethash x class-system))) (rest classes)))
          (appended-names (apply #'append superclasses-names)))
     (remove-duplicates (append (list (car classes)) appended-names) :from-end t )))
 
+;;;;;;
+(defun get-precedence-list2 (classes)
+  (let* ((superclasses-names
+	  (mapcar #'(lambda (x) (gethash 'is-a (gethash x class-system))) (rest classes)))
+         (appended-names (apply #'append superclasses-names)))
+    appended-names))
+;;;;;;
 
 (defmacro def-class (classes-names &rest slots)
   (let*
       ((subclass? (listp classes-names))
        (class-name
-         (if subclass?
-             (car classes-names)
-             classes-names))
+	(if subclass?
+	    (car classes-names)
+	    classes-names))
        (precedence-list
-         (if subclass?
-             (get-precedence-list classes-names)
-             (list class-name)))
+	(if subclass?
+	    (get-precedence-list classes-names)
+	    (list class-name)))
+       ;;;;;;;;;;;;;;;;;;;;;
+       (precedence-list2
+	(if subclass?
+	    (get-precedence-list2 classes-names)
+	    (list class-name)))
+       ;;;;;;;;;;;;;;;;;;;;;
        (class-slots
-         (if subclass?
-             (get-class-slots (rest classes-names) slots)
-             slots))
+	(if subclass?
+	    (get-class-slots (rest classes-names) slots)
+	    slots))
        (class-slots-names
-         (mapcar #'(lambda (x)
-                     (if (listp x)
-                         (car x)
-                         x)) class-slots)))
+	(mapcar #'(lambda (x)
+		    (if (listp x)
+			(car x)
+			x)) class-slots)))
     `(progn
        ,(format t "Is subclass?               -> ~a~%" subclass?)
        ,(format t "This class name            -> ~a~%" class-name)
        ,(format t "This class precedence-list -> ~a~%" precedence-list)
+       ,(format t "This class precedence-list2 -> ~a~%" precedence-list2);;;;;;;;
        ,(format t "This class slots           -> ~a~%" class-slots)
-       ,(add-to-class-system class-name class-slots precedence-list)
+       ,(add-to-class-system class-name class-slots precedence-list precedence-list2);;;;
        ,(create-constructor class-name class-slots-names class-slots)
        ,@(mapcar 'create-getter
                  (make-list (length class-slots-names) :initial-element class-name)
